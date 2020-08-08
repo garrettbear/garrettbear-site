499c8ea334f5a946e8a6050c9574669c06b922a4
Garrett Bear <gwhisten@gmail.com>
2020-08-07 17:59:59 -0700

    page create section 7

--- four/src/pages/404.js
+++ four/src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default function About({ data }) {
+export default function Page404({ data }) {
   return (
     <Layout>
       <h1>About {data.site.siteMetadata.title}</h1>
