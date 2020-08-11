73c6f208bd90af7fa190122b08b65c16fbc9efd2
Garrett Bear <gwhisten@gmail.com>
2020-08-11 09:59:52 -0700

    markdown posts

--- four/src/templates/blog-post.js
+++ four/src/templates/blog-post.js
@@ -1,10 +1,32 @@
 import React from "react"
+import { graphql } from "gatsby" // highlight-line
 import Layout from "../components/layout"
 
-export default function BlogPost() {
+// highlight-start
+export default function BlogPost({ data }) {
+  const post = data.markdownRemark
+  // highlight-end
   return (
     <Layout>
-      <div>Hello blog post</div>
+      {/* highlight-start */}
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+      {/* highlight-end */}
     </Layout>
   )
 }
+
+// highlight-start
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
+// highlight-end
